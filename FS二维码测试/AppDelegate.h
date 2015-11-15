71d189c1162d6cc8889c2f5d27b16e1b62dbe8aa
FsThatOne <fsymbio@icloud.com>
2015-11-15 16:01:07 +0800

    建项目

---
+++ FS二维码测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FS二维码测试
+//
+//  Created by FS小一 on 15/11/15.
+//  Copyright © 2015年 FSxiaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FS二维码测试/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FS二维码测试
+//
+//  Created by FS小一 on 15/11/15.
+//  Copyright © 2015年 FSxiaoyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
